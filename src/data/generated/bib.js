8b3c619b16d8cb3d4b12e3f7ea1e0942abce9177
gaohao-WQ <alyhg14@nottingham.ac.uk>
2025-05-15 16:38:05 +0100

    Update bib.js

--- src/data/generated/bib.js
+++ src/data/generated/bib.js
@@ -9,5 +9,27 @@ const generatedBibEntries = {
     "year": "2016",
     "doi": "10.14257/ijsh.2016.10.6.16",
     "type": "article"
+  },
+   "agarwal2016smartlock": {
+    "title": "Smart Door Lock System for Elderly, Handicapped People Living Alone",
+    "author": "Agarwal, Ateev and Mehandiratta, Eesh and Sanket, Rohit and Samkaria, Rohit and Gupta, Tanisha and Singh, Rajesh and Gehlot, Anita",
+    "journal": "International Journal of Smart Home",
+    "volume": "10",
+    "number": "6",
+    "pages": "155--162",
+    "year": "2016",
+    "doi": "10.14257/ijsh.2016.10.6.16",
+    "type": "article"
+  },
+   "agarwal2016smartlock": {
+    "title": "Smart Door Lock System for Elderly, Handicapped People Living Alone",
+    "author": "Agarwal, Ateev and Mehandiratta, Eesh and Sanket, Rohit and Samkaria, Rohit and Gupta, Tanisha and Singh, Rajesh and Gehlot, Anita",
+    "journal": "International Journal of Smart Home",
+    "volume": "10",
+    "number": "6",
+    "pages": "155--162",
+    "year": "2016",
+    "doi": "10.14257/ijsh.2016.10.6.16",
+    "type": "article"
   }
 };
